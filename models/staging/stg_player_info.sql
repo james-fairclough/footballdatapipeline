0e8196abe001742daf6fe725b696924d75601bab
James Fairclough <j_fairclough@ymail.com>
2025-06-10 12:10:49 +0000

    upadting issue with dupes in player info staging

--- models/staging/stg_player_info.sql
+++ models/staging/stg_player_info.sql
@@ -1,6 +1,11 @@
 {{ config(materialized='table') }}
 
 
-SELECT DISTINCT *
-FROM ({{ get_players_table() }}) 
-
+WITH ranked_players AS (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY player_age desc) AS row_num
+    FROM ({{ get_players_table() }}) 
+)
+SELECT * 
+FROM ranked_players 
+WHERE row_num = 1
