f34166b211a54091c0ef0d3edc3d801dc21cd021
James Fairclough <j_fairclough@ymail.com>
2025-06-10 18:00:59 +0000

    adding leagues

--- models/staging/stg_leagues.sql
+++ models/staging/stg_leagues.sql
@@ -1,14 +1,4 @@
 {{ config(materialized='table') }}
 
-with source as (
-    select * from {{ source('raw', 'Leagues') }}
-),
 
-unique_leagues as (
-    select
-        *
-    from source where league_id is not null
-)
-
-select * from unique_leagues
-
+SELECT * FROM ({{ get_leagues_table() }})
